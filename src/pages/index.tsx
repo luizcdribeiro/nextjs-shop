f82417c29d42c6d3a562ad7bd27448dfddb7cf79
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2023-04-21 13:00:47 -0300

    Adicionando slides

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
-import { HomerContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
+import { useKeenSlider } from 'keen-slider/react';
+
+import { HomerContainer, Product } from "@/styles/pages/home";
+
+import 'keen-slider/keen-slider.min.css';
+
 import camiseta1 from '../assets/camisetas/01.png'
 import camiseta2 from '../assets/camisetas/02.png'
 import camiseta3 from '../assets/camisetas/03.png'
 
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomerContainer>
-      <Product>
+    <HomerContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480}/>
         <footer>
           <strong>Camiseta X</strong>
@@ -17,11 +30,38 @@ export default function Home() {
 
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480}/>
         <footer>
           <strong>Camiseta W</strong>
           <span>R$ 89,90</span>
+        </footer>
+
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta J</strong>
+          <span>R$ 99,90</span>
+        </footer>
+
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta J</strong>
+          <span>R$ 99,90</span>
+        </footer>
+
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta J</strong>
+          <span>R$ 99,90</span>
         </footer>
 
       </Product>
