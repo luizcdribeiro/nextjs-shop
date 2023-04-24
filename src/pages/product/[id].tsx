36ad50add11de9f45696aa96778ecd6b84b032ea
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2023-04-24 17:57:03 -0300

    adicionado função de checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,12 +1,34 @@
 import {  ProductPropsItems } from "@/interfaces/products";
 import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Stripe from "stripe";
 
 export default function Product({ product }: ProductPropsItems) {
+
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl
+    }
+    catch(error) {
+      setIsCreatingCheckoutSession(false)
+
+      alert('Falha ao redirecionar checkout!')
+    }
+  }
 
   const { isFallback } = useRouter()
 
@@ -26,7 +48,7 @@ export default function Product({ product }: ProductPropsItems) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
